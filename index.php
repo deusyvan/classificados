d441965c1b8565bc50ed1f91cb169c3522e56725
deusyvan <deusyvan@gmail.com>
2018-07-02 22:59:30 -0300

    Primeiro commit com estrutura simples de sistema

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Classificados</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ script.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA `classificados` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `classificados`.`usuarios` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(32) NOT NULL,
+  `telefone` VARCHAR(30) NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `classificados`.`categorias` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `classificados`.`anuncios` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT(11) NOT NULL,
+  `id_categoria` INT(11) NOT NULL,
+  `titulo` VARCHAR(100) NULL DEFAULT NULL,
+  `descricao` TEXT NULL,
+  `valor` FLOAT NULL DEFAULT NULL,
+  `estado` INT(11) NULL,
+  PRIMARY KEY (`id`));
+
+
+
+
+
+
+
+
+
