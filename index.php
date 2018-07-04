16789e82191e742455c50adb6f81fb9fe0c9bf4c
deusyvan <deusyvan@gmail.com>
2018-07-03 23:04:02 -0300

    Aplicando conteúdo

--- index.php
+++ index.php
@@ -29,6 +29,11 @@
 			<h2>Nós temos hoje 890 anúncios</h2>
 			<p>E mais de 861 usuários cadastrados</p>
 		</div>
+		
+		<div class="row">
+			<div class="col-sm-3">...</div>
+			<div class="col-sm-9">...</div>
+		</div>
 	</div>
 
 </body>
