b68392c95ef88979ce9c864dff5bedc4e89e3ef5
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-05 16:29:28 -0300

    Aqui realmente cadastra o usuário

--- classes/usuarios.class.php
+++ classes/usuarios.class.php
@@ -2,7 +2,7 @@
 class Usuarios {
     
     public function cadastrar($nome, $email,$senha, $telefone){
-        
+        global $pdo;
         $sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email");
         $sql->bindValue(":email", $email);
         $sql->execute();
