25ca06632498b3ec25161a7cba6bf2d0e5d3c87b
deusyvan <deusyvan@gmail.com>
2018-07-05 00:48:59 -0300

    Se algum não estiver envia msg de alerta

--- cadastre-se.php
+++ cadastre-se.php
@@ -13,7 +13,9 @@
         if(!empty($nome) && !empty($email) && !empty($senha)){
             $u->cadastrar($nome, $email, $senha, $telefone);
         } else {
-            
+            ?>
+            <div class="alert alert-warning"> Preencha todos os campos</div>
+            <?php 
         }
     }
     ?>
@@ -35,7 +37,7 @@
 			<label for="telefone">Telefone: </label>
 			<input type="text" name="telefone" id="telefone" class="form-control" />
 		</div>
-		<input type="submit" value="Cadastrar" class="btn btn-default">
+		<input type="submit" value="Cadastrar" class="btn btn-default"/>
 	</form>
 </div>
 <?php require 'pages/footer.php'; ?>
