6584afad71f2229cf966027f8e449893e27561c6
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-05 16:16:48 -0300

    Fizemos toda a lógica do cadastre-se para cadastro ou quando já existe

--- classes/usuarios.class.php
+++ classes/usuarios.class.php
@@ -1,7 +1,19 @@
 <?php 
 class Usuarios {
     
-    
+    public function cadastrar($nome, $email,$senha, $telefone){
+        
+        $sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email");
+        $sql->bindValue(":email", $email);
+        $sql->execute();
+        
+        if ($sql->rowCount() == 0) {
+            //cadastra
+        } else {
+            return false;
+            
+        }
+    }
 }
 
 ?>
