f1b4655183892ef9ce87b0e56566d8c18ad6de85
deusyvan <deusyvan@gmail.com>
2018-07-07 11:04:02 -0300

    Criando nossa consulta no banco de dados para buscar todos os anuncios do usuario

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -2,8 +2,18 @@
  
 class Anuncios {
     public function getMeusAnuncios(){
+        global $pdo;
         
+        $array = array();
+        $sql = $pdo->query("SELECT * FROM anuncios where id_usuario  = :id_usuario");
+        $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
+        $sql->execute();
         
+        if ($sql->rowCount() > 0){
+            $array = $sql->fetchAll();
+        }
+        
+        return $array;
     }
 }
 ?>
