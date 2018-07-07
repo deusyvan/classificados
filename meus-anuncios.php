b1bc2d693fa1e33eb15bc783523fd62492f06d15
deusyvan <deusyvan@gmail.com>
2018-07-07 10:53:44 -0300

    Mostrando como vamos buscar a imagem do anuncio

--- meus-anuncios.php
+++ meus-anuncios.php
@@ -26,7 +26,7 @@ if (empty($_SESSION['cLogin'])){
             foreach ($anuncios as $anuncio):
 			?>
 			<tr>
-				<td><img src="" border="0" /></td>
+				<td><img src="assets/images/anuncios/<?php echo $anuncio['url'];?>" border="0" /></td>
 				<td><?php echo $anuncio['titulo']; ?></td>
 				<td>R$ <?php echo number_format($anuncio['valor'], 2); ?></td>
 				<td></td>		
