15623f9015aa75191ef5d094df400b08a3adb652
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-11 16:10:56 -0300

    Fazendo com que fique selecionado o combo que foi selecionado anteriormente após clicar em buscar

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $categorias = $c->getLista();
 						<select id="categoria" name="filtros[categoria]" class="form-control">
 							<option></option>
 							<?php foreach ($categorias as $cat): ?>
-								<option value="<?php echo $cat['id']; ?>"><?php echo $cat['nome']; ?></option>
+								<option value="<?php echo $cat['id']; ?>" <?php echo ($cat['id']==$filtros['categoria'])? 'selected="selected"' : ''; ?>><?php echo $cat['nome']; ?></option>
 							<?php endforeach; ?>
 						</select>
 					</div>
@@ -56,20 +56,20 @@ $categorias = $c->getLista();
 						<label for="preco">Preço:</label>
 						<select id="preco" name="filtros[preco]" class="form-control">
 							<option></option>
-							<option value="0-50">R$ 0 - 50</option>
-							<option value="51-100">R$ 51 - 100</option>
-							<option value="101-200">R$ 101 - 200</option>
-							<option value="201-500">R$ 201 - 500</option>
-							<option value="+500">acima de R$ 500</option>
+							<option value="0-50" <?php echo ($filtros['preco'] == '0-50')? 'selected="selected"' : ''; ?>>R$ 0 - 50</option>
+							<option value="51-100" <?php echo ($filtros['preco'] == '51-100')? 'selected="selected"' : ''; ?>>R$ 51 - 100</option>
+							<option value="101-200" <?php echo ($filtros['preco'] == '101-200')? 'selected="selected"' : ''; ?>>R$ 101 - 200</option>
+							<option value="201-500" <?php echo ($filtros['preco'] == '201-500')? 'selected="selected"' : ''; ?>>R$ 201 - 500</option>
+							<option value="+500" <?php echo ($filtros['preco'] == '+500')? 'selected="selected"' : ''; ?>>acima de R$ 500</option>
 						</select>
 					</div>
 					<div class="form-group">
 						<label for="estado">Estado de Conservação:</label>
 						<select id="estado" name="filtros[estado]" class="form-control">
 							<option></option>
-							<option value="0">Ruim</option>
-							<option value="1">Bom</option>
-							<option value="2">Ótimo</option>
+							<option value="0" <?php echo ($filtros['estado'] == '0')? 'selected="selected"' : ''; ?>>Ruim</option>
+							<option value="1" <?php echo ($filtros['estado'] == '1')? 'selected="selected"' : ''; ?>>Bom</option>
+							<option value="2" <?php echo ($filtros['estado'] == '2')? 'selected="selected"' : ''; ?>>Ótimo</option>
 						</select>
 					</div>
 					<div class="form-group">
