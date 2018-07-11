5212ce966af392540c241f336421bd98b89a0527
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-11 13:24:23 -0300

    Criando produto

---
+++ produto.php
@@ -0,0 +1,19 @@
+<?php  require 'pages/header.php'; ?>
+<?php 
+require 'classes/anuncios.class.php';
+require 'classes/usuarios.class.php';
+$a = new Anuncios();
+$u = new Usuarios();
+
+?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-4">
+				...
+			</div>
+			<div class="col-sm-8">
+				...
+			</div>
+		</div>
+	</div>
+<?php require 'pages/footer.php'; ?>
