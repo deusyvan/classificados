2d4fdd7384abdf548b99442b1a02120001956bd3
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-11 14:00:40 -0300

    Estrutura dos dados do produto

--- produto.php
+++ produto.php
@@ -17,7 +17,7 @@ $info = $a->getAnuncio($id);
 ?>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-4">
+			<div class="col-sm-5">
 				<div class="carousel slide" data-ride="carousel" id="meuCarousel">
 					<div class="carousel-inner" role="listbox">
 						<?php foreach ($info['fotos'] as $chave => $foto): ?>
@@ -26,12 +26,18 @@ $info = $a->getAnuncio($id);
 							</div>
 						<?php endforeach; ?>
 					</div>
-					<a class="left carousel-control" href="#meuCarousel" role="button" data-slide="prev"><span><</span></a>		
-					<a class="right carousel-control" href="#meuCarousel" role="button" data-slide="next"><span>></span></a>	
+					<a class="left carousel-control" href="#meuCarousel" role="button" data-slide="prev"><span> < </span></a>		
+					<a class="right carousel-control" href="#meuCarousel" role="button" data-slide="next"><span> > </span></a>	
+				</div>
 			</div>
-			<div class="col-sm-8">
-				...
-			</div>
+			<div class="col-sm-7">
+    			<h1>Nome do produto</h1>
+    			<h4>Categoria do Produto</h4>
+    			<p>Descrição do Produto</p>
+    			<br/>
+    			<h3>R$ 999</h3>
+    			<h4>Telefone: 35335353</h4>
+    		</div>
 		</div>
 	</div>
 <?php require 'pages/footer.php'; ?>
