65ed785d181c5903e0190128dfae839cdca204e6
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-11 11:38:39 -0300

    Realizamos uma verificação na classe para ativar a página que estiver selecionada

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $anuncios = $a->getUltimosAnuncios($p, $por_pagina);
 				
 				<ul class="pagination">
 					<?php for ($i=1; $i<=$total_paginas; $i++): ?>
-						<li><a href="index.php?p=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+						<li class="<?php echo ($p==$i)?'active':''; ?>"><a href="index.php?p=<?php echo $i; ?>"><?php echo $i; ?></a></li>
 					<?php endfor; ?>
 				</ul>
 			</div>
