230a9baeecd5bba9065fc3a3897a1f08d918bb6a
deusyvan <deusyvan@gmail.com>
2018-07-15 23:52:08 -0300

    Tornando categorias um model

--- models/Categorias.php
+++ models/Categorias.php
@@ -1,11 +1,9 @@
 <?php 
-class Categorias {
+class Categorias extends model{
     
     public function getLista(){
         $array = array();
-        global $pdo;
-        
-        $sql = $pdo->query("SELECT * FROM categorias");
+        $sql = $this->db->query("SELECT * FROM categorias");
         if($sql->rowCount() > 0){
             $array = $sql->fetchAll();
         }
