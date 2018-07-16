7c5dfa93b2704a172a41556319163bb34314cdeb
deusyvan <deusyvan@gmail.com>
2018-07-15 23:54:15 -0300

    Tornando usuarios model

--- models/Usuarios.php
+++ models/Usuarios.php
@@ -1,23 +1,20 @@
 <?php 
-class Usuarios {
+class Usuarios extends model{
     
     public function getTotalUsuarios(){
-        global $pdo;
-        
-        $sql = $pdo->query("SELECT COUNT(*) as c FROM usuarios");
+        $sql = $this->db->query("SELECT COUNT(*) as c FROM usuarios");
         $row = $sql->fetch();
         return $row['c'];
         
     }
     
     public function cadastrar($nome, $email,$senha, $telefone){
-        global $pdo;
-        $sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email");
+        $sql = $this->db->prepare("SELECT id FROM usuarios WHERE email = :email");
         $sql->bindValue(":email", $email);
         $sql->execute();
         
         if ($sql->rowCount() == 0) {
-            $sql = $pdo->prepare("INSERT INTO usuarios SET nome = :nome, email = :email, senha = :senha, telefone = :telefone");
+            $sql = $this->db->prepare("INSERT INTO usuarios SET nome = :nome, email = :email, senha = :senha, telefone = :telefone");
             $sql->bindValue(":nome", $nome);
             $sql->bindValue(":email", $email);
             $sql->bindValue(":senha", md5($senha));
@@ -33,9 +30,7 @@ class Usuarios {
     }
     
     public function login ($email, $senha){
-      global $pdo;
-      
-      $sql = $pdo->prepare("SELECT id,nome FROM usuarios WHERE email = :email AND senha = :senha");
+      $sql = $this->db->prepare("SELECT id,nome FROM usuarios WHERE email = :email AND senha = :senha");
       $sql->bindValue(":email", $email);
       $sql->bindValue(":senha", md5($senha));
       $sql->execute();
