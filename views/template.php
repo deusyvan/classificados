441cad28722a2caa88a0e958b1020315ab5e2c58
deusyvan <deusyvan@gmail.com>
2018-07-15 23:33:28 -0300

    Adicionamos em template apenas o que é fixo

--- views/template.php
+++ views/template.php
@@ -6,7 +6,23 @@
 	</head>
 	
 	<body>
-		
+		<nav class="navbar navbar-inverse">
+    		<div class="container-fluid">
+    			<div class="navbar-header">
+    				<a href="./" class="navbar-brand">Classificados</a>
+    			</div>
+    			<ul class="nav navbar-nav navbar-right">
+    				<?php if (isset($_SESSION['cLogin']) && !empty($_SESSION['cLogin'])):?>
+    					<li><a href="#">Usuário: <?php echo $_SESSION['cNome']; ?></a></li>
+    					<li><a href="meus-anuncios.php">Meus Anúncios</a></li>
+    				    <li><a href="sair.php">Sair</a></li>
+    				<?php  else:?>
+        				<li><a href="cadastre-se.php">Cadastre-se</a></li>
+        				<li><a href="login.php">Login</a></li>
+    				<?php endif; ?>
+    			</ul>
+    		</div>
+    	</nav>
 		
 		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
 		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js" ></script>
