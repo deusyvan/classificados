5651c00945f470d09acb79b54b5267b0a4f45572
deusyvan <deusyvan@gmail.com>
2018-07-15 23:24:25 -0300

    Limpando um pouco mais arquivos que não serão nescessários, lembrando que em assets já deixamos o que vamos usar

--- views/template.php
+++ views/template.php
@@ -6,10 +6,7 @@
 	</head>
 	
 	<body>
-		<h1>Este é o topo</h1>
-		<a href="<?php echo BASE_URL; ?>">Home</a>
-		<a href="<?php echo BASE_URL; ?>galeria">Galeria</a>
-		<hr/>
+		
 		
 		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
 		
