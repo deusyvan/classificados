f809de56062a105a6386e17c87a075906d9b2aac
deusyvan <deusyvan@gmail.com>
2018-07-15 23:28:29 -0300

    Inserindo css e js no template

--- views/template.php
+++ views/template.php
@@ -1,14 +1,16 @@
 <html>
 	<head>
 		<title>Meu Site</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/style.css" />
-		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/javascript.js" ></script>
 	</head>
 	
 	<body>
 		
 		
 		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
-		
+		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js" ></script>
+		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js" ></script>
+		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/script.js" ></script>
 	</body>
 </html>
