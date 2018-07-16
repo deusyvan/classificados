66e8a56ce7dd0973bbf168506480d335857a3336
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-16 13:42:24 -0300

    Buscamos as informações do anuncio pelo getAnuncio e o id preenchidos

---
+++ controllers/produtoController.php
@@ -0,0 +1,23 @@
+<?php 
+
+class produtoController extends controller{
+    
+    public function index(){
+        
+    }
+    
+    public function abrir($id){
+        
+        $a = new Anuncios();
+        $u = new Usuarios();
+        
+        if(empty($id)){
+            header("Location: ".BASE_URL);
+            exit;
+           } 
+        
+        $info = $a->getAnuncio($id);
+        
+        
+    }
+}
