2ee83b1558f88f0b2c687d52f8895256d57e204a
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-16 13:55:32 -0300

    Corrigindo links e mandando para view

--- views/template.php
+++ views/template.php
@@ -9,16 +9,16 @@
 		<nav class="navbar navbar-inverse">
     		<div class="container-fluid">
     			<div class="navbar-header">
-    				<a href="./" class="navbar-brand">Classificados</a>
+    				<a href="<?php echo BASE_URL; ?>" class="navbar-brand">Classificados</a>
     			</div>
     			<ul class="nav navbar-nav navbar-right">
     				<?php if (isset($_SESSION['cLogin']) && !empty($_SESSION['cLogin'])):?>
     					<li><a href="#">Usuário: <?php echo $_SESSION['cNome']; ?></a></li>
-    					<li><a href="meus-anuncios.php">Meus Anúncios</a></li>
-    				    <li><a href="sair.php">Sair</a></li>
+    					<li><a href="<?php echo BASE_URL; ?>anuncios">Meus Anúncios</a></li>
+    				    <li><a href="<?php echo BASE_URL; ?>/login/sair">Sair</a></li>
     				<?php  else:?>
-        				<li><a href="cadastre-se.php">Cadastre-se</a></li>
-        				<li><a href="login.php">Login</a></li>
+        				<li><a href="<?php echo BASE_URL; ?>cadastrar">Cadastre-se</a></li>
+        				<li><a href="<?php echo BASE_URL; ?>login">Login</a></li>
     				<?php endif; ?>
     			</ul>
     		</div>
