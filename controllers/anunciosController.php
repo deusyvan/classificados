cdcaae88d03eb6a68f622430c4148f984264ebd6
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-16 16:24:00 -0300

    Criando o anuncios controller e as view anuncios e add

---
+++ controllers/anunciosController.php
@@ -0,0 +1,47 @@
+<?php
+class anunciosController extends controller{
+    
+    public function index(){
+      $dados = array();
+     
+      if (empty($_SESSION['cLogin'])){
+          header("Location: ".BASE_URL."login");
+          exit;
+      } 
+      
+      $a = new Anuncios();
+      $anuncios = $a->getMeusAnuncios();
+      $dados['anuncios'] = $anuncios;
+      
+      $this->loadTemplate('anuncios', $dados);
+    }
+    
+    public function add(){
+        $dados = array();
+        
+        if(empty($_SESSION['cLogin'])) {
+            header("Location: ".BASE_URL."login");
+            exit;
+        }
+        
+        $a = new Anuncios();
+        if(isset($_POST['titulo']) && !empty($_POST['titulo'])) {
+            $titulo = addslashes($_POST['titulo']);
+            $categoria = addslashes($_POST['categoria']);
+            $valor = addslashes($_POST['valor']);
+            $descricao = addslashes($_POST['descricao']);
+            $estado = addslashes($_POST['estado']);
+            
+            $a->addAnuncio($titulo, $categoria, $valor, $descricao, $estado);
+            $dados['msg'] = '<div class="alert alert-success">
+                                Produto adicionado com sucesso!
+                             </div>';
+        }
+        
+        $c = new Categorias();
+        $categorias = $c->getLista();
+        $dados['categorias'] = $categorias;
+        
+        $this->loadTemplate('add',$dados);
+   }
+}
