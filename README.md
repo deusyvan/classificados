1792035d567d3f99f84bbf7da575f3781419bc8c
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-16 10:31:37 -0300

    Colamos aqui pois é responsabilidade do controller fazer os filtros consultas e outras coisas do model

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Sistema de classificados tipo OLX
 
 * Neste sistema de estudo veremos a construção de um sistema básico de classificados, criaremos um template simples para todas 
 as páginas.
-* Não usaremos a estrura de MVC, mas seguiremos com os princípios de Orientação a Objetos, que servirá de base para a construção 
+* Não usaremos a estrura de MVC na primeira versão, mas seguiremos com os princípios de Orientação a Objetos, que servirá de base para a construção 
 futura com MVC.
+* Conclusão: Estaremos convertendo todo o sistema para a nova versão 2, onde irá contemplar a Orientação a Objetos seguindo os conceitos básicos do MVC.
--- controllers/homeController.php
+++ controllers/homeController.php
@@ -3,6 +3,33 @@ class homeController extends controller{
     
     public function index(){
         $dados = array();
+        $a = new Anuncios();
+        $u = new Usuarios();
+        $c = new Categorias();
+        
+        $filtros = array(
+            'categoria' => '',
+            'preco' => '',
+            'estado' => ''
+        );
+        
+        if (isset($_GET['filtros'])) {
+            $filtros = $_GET['filtros'];
+        }
+        
+        $total_anuncios = $a->getTotalAnuncios($filtros);
+        $total_usuarios = $u->getTotalUsuarios();
+        
+        $p = 1;
+        if (isset($_GET['p']) && !empty($_GET['p'])){
+            $p = addslashes($_GET['p']);
+        }
+        
+        $por_pagina = 2;
+        $total_paginas = ceil($total_anuncios / $por_pagina);
+        
+        $anuncios = $a->getUltimosAnuncios($p, $por_pagina, $filtros);
+        $categorias = $c->getLista();
       
         $this->loadTemplate('home', $dados);
         
